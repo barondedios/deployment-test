03674fdf1561223b55c34e8899f160c977bafd8c
Baron <realbarondedios@gmail.com>
2025-06-06 02:55:58 +0800

    last

--- routes/console.php
+++ routes/console.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schedule;
+// use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
